d0cad0852e9906afa7f0d07dfb5ab576802a055e
davidsamuel23 <91531206+davidsamuel23@users.noreply.github.com>
2021-10-20 14:01:23 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,3 +1,4 @@
+
 player1_name = localStorage.getItem("player1_name");
 player2_name = localStorage.getItem("player2_name");
 
@@ -14,45 +15,29 @@ document.getElementById("player_question").innerHTML = "Question Turn - " + play
 document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
 
 function send() {
-get_word = document.getElementById("word").value;
-word = get_word.toLowerCase();
-console.log("word in lowerCase = " + word);
+number1 = document.getElementById("number1").value;
+number2 = document.getElementById("number2").value;
+actual_answer = parseInt(number1) * parseInt(number2);
+console.log(actual_answer);
 
-    charAt1 = word.charAt(1);
-console.log(charAt1);
 
-lenght_divide_2 = Math.floor(word.length/2);
-charAt2 = word.charAt(lenght_divide_2);
-console.log(charAt2);
-
-    lenght_minus_1 = word.length - 1;
-    charAt3 = word.charAt(lenght_minus_1);
-console.log(charAt3);
-
-    remove_charAt1 = word.replace(charAt1, "_");
-    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
-    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
-console.log(remove_charAt3);
-
-    question_word = "<h4 id='word_display'> Q. "+remove_charAt3+"</h4>";
+    question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
-    row =  question_word + input_box + check_button ;
+    row =  question_number + input_box + check_button ;
     document.getElementById("output").innerHTML = row;
-document.getElementById("word").value = "";
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
 }
 
 
 question_turn = "player1";
 answer_turn = "player2";
 
-
 function check()
 {
 get_answer = document.getElementById("input_check_box").value;
-answer = get_answer.toLowerCase();
-console.log("answer in lower case - " + answer);
-if(answer == word)
+if(get_answer == actual_answer)
 {
 if(answer_turn == "player1")
 {
@@ -65,6 +50,7 @@ player2_score = player2_score +1;
    document.getElementById("player2_score").innerHTML = player2_score;
 }
 }
+
 if(question_turn == "player1")
 {
 question_turn = "player2"
